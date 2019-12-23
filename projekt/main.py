167c78b7f28ae45e710dc89a31926c450840daa4
Katarzyna Winkler <kasia.winkler99@gmail.com>
2019-12-23 21:03:32 +0100

    moving blocks only arrow up(prepare background)

--- projekt/main.py
+++ projekt/main.py
@@ -3,6 +3,31 @@ import random
 import os.path
 from pygame.locals import *
 
+# color
+grey = (0x99, 0x99, 0x99)
+black = (0x00, 0x00, 0x00)
+
+# text
+text = 'Gra 2048'
+
+# loading images
+image_2 = pygame.image.load(os.path.join("Number", "2.bmp"))
+image_4 = pygame.image.load(os.path.join("Number", "4.bmp"))
+image_8 = pygame.image.load(os.path.join("Number", "8.bmp"))
+image_16 = pygame.image.load(os.path.join("Number", "16.bmp"))
+image_32 = pygame.image.load(os.path.join("Number", "32.bmp"))
+image_64 = pygame.image.load(os.path.join("Number", "64.bmp"))
+image_128 = pygame.image.load(os.path.join("Number", "128.bmp"))
+image_256 = pygame.image.load(os.path.join("Number", "256.bmp"))
+image_512 = pygame.image.load(os.path.join("Number", "512.bmp"))
+image_1024 = pygame.image.load(os.path.join("Number", "1024.bmp"))
+image_2048 = pygame.image.load(os.path.join("Number", "2048.bmp"))
+image_lose = pygame.image.load(os.path.join("Number", "Przegrales.jpg"))
+image_win = pygame.image.load(os.path.join("Number", "Wygrales.jpg"))
+
+# date
+FPS = 90
+fpsClock = pygame.time.Clock()
 
 
 def DrawBoard(x, y, height, screen, color, line):
@@ -30,12 +55,43 @@ def END(screen, image, lista):
          return True
 
 ##ARROW
-def arrow_up(lista):
+def arrow_up(lista,screen):
    for i in range(3):
       for j in range(3):
+
          if lista[j][i] == 0:
             for k in range(j + 1, 3):
                if lista[k][i] != 0:
+                  (x, y) = checking_position(k, i)
+                  z = y
+                  while y != z - 100:
+                     y-=4
+                     if lista[k][i] == 2:
+                        screen.blit(image_2, (x,y))
+                     if lista[k][i] == 4:
+                        screen.blit(image_4, (x,y))
+                     if lista[k][i] == 8:
+                        screen.blit(image_8, (x,y))
+                     if lista[k][i] == 16:
+                        screen.blit(image_16, (x,y))
+                     if lista[k][i] == 32:
+                        screen.blit(image_32, (x,y))
+                     if lista[k][i] == 64:
+                        screen.blit(image_64, (x,y))
+                     if lista[k][i] == 128:
+                        screen.blit(image_128, (x,y))
+                     if lista[k][i] == 256:
+                        screen.blit(image_256, (x,y))
+                     if lista[k][i] == 512:
+                        screen.blit(image_512, (x,y))
+                     if lista[k][i] == 1024:
+                        screen.blit(image_1024, (x,y))
+                     if lista[k][i] == 2048:
+                        screen.blit(image_2048, (x,y))
+                        screen.blit(image_win, (0, 100))
+                     pygame.display.update()
+                     fpsClock.tick(FPS)
+
                   lista[j][i] = lista[k][i]
                   lista[k][i] = 0
                   break
@@ -118,30 +174,71 @@ def time(screen):
       time = font.render(str((pygame.time.get_ticks()//1000)//60) + ':' + '0' + str((pygame.time.get_ticks()//1000)%60), 1, (250, 250, 250))
    else:
       time = font.render(str((pygame.time.get_ticks()//1000)//60) + ':' + str((pygame.time.get_ticks() // 1000) % 60), 1,(250, 250, 250))
+
    screen.blit(time, (60, 80))
 
+def image(lista_main,screen,i,j):
+
+   position = checking_position(i,j)
+
+   if lista_main[i][j] == 2:
+      screen.blit(image_2, position)
+   if lista_main[i][j] == 4:
+      screen.blit(image_4, position)
+   if lista_main[i][j] == 8:
+      screen.blit(image_8, position)
+   if lista_main[i][j] == 16:
+      screen.blit(image_16, position)
+   if lista_main[i][j] == 32:
+      screen.blit(image_32, position)
+   if lista_main[i][j] == 64:
+      screen.blit(image_64, position)
+   if lista_main[i][j] == 128:
+      screen.blit(image_128, position)
+   if lista_main[i][j] == 256:
+      screen.blit(image_256, position)
+   if lista_main[i][j] == 512:
+      screen.blit(image_512, position)
+   if lista_main[i][j] == 1024:
+      screen.blit(image_1024, position)
+   if lista_main[i][j] == 2048:
+      screen.blit(image_2048, position)
+      screen.blit(image_win, (0, 100))
+
+
+def first_block(lista):
+   #draw first block
+   x=random.choice([0,100,200])
+   y=random.choice([100,200,300])
+
+   #safe first block to the list
+   for j in range(0,3,1):
+      for n in range(len(lista[j])):
+         if (y == 100 and j == 0) or (y == 200 and j == 1) or (y == 300 and j == 2):
+            if y == 100 and j == 0:
+               if (x == 0 and n == 0) or (x == 100 and n == 1) or (x == 200 and n == 2):
+                  lista[j][n] = 2
+            if y == 200 and j == 1:
+               if (x == 0 and n == 0) or (x == 100 and n == 1) or (x == 200 and n == 2):
+                  lista[j][n] = 2
+            if y == 300 and j == 2:
+               if (x == 0 and n == 0) or (x == 100 and n == 1) or (x == 200 and n == 2):
+                  lista[j][n] = 2
 
 ##GAME
 def main():
-   # color
-   grey = (0x99, 0x99, 0x99)
-   black = (0x00, 0x00, 0x00)
-
    # useful data
    (width, height) = (300, 400)
-   lista1 = [0, 0, 0]
-   lista2 = [0, 0, 0]
-   lista3 = [0, 0, 0]
-   lista_main = [lista1, lista2, lista3]
    exit = 0
    move = 0
    copy = []
    copy_score = ''
    safe = []
    points = 0
-
-   # text
-   text = 'Gra 2048'
+   lista1 = [0, 0, 0]
+   lista2 = [0, 0, 0]
+   lista3 = [0, 0, 0]
+   lista_main = [lista1, lista2, lista3]
 
    #pygame init
    pygame.init()
@@ -160,44 +257,15 @@ def main():
 
 
 
-   #loading images
-   image_2 = pygame.image.load(os.path.join("Number", "2.bmp"))
-   image_4 = pygame.image.load(os.path.join("Number", "4.bmp"))
-   image_8 = pygame.image.load(os.path.join("Number", "8.bmp"))
-   image_16 = pygame.image.load(os.path.join("Number", "16.bmp"))
-   image_32 = pygame.image.load(os.path.join("Number", "32.bmp"))
-   image_64 = pygame.image.load(os.path.join("Number", "64.bmp"))
-   image_128 = pygame.image.load(os.path.join("Number", "128.bmp"))
-   image_256 = pygame.image.load(os.path.join("Number", "256.bmp"))
-   image_512 = pygame.image.load(os.path.join("Number", "512.bmp"))
-   image_1024 = pygame.image.load(os.path.join("Number", "1024.bmp"))
-   image_2048 = pygame.image.load(os.path.join("Number", "2048.bmp"))
-   image_lose = pygame.image.load(os.path.join("Number", "Przegrales.jpg"))
-   image_win = pygame.image.load(os.path.join("Number", "Wygrales.jpg"))
 
-   #draw first block
-   x=random.choice([0,100,200])
-   y=random.choice([100,200,300])
 
-   #safe first block to the list
-   for j in range(0,3,1):
-      for n in range(len(lista_main[j])):
-         if (y == 100 and j == 0) or (y == 200 and j == 1) or (y == 300 and j == 2):
-            if y == 100 and j == 0:
-               if (x == 0 and n == 0) or (x == 100 and n == 1) or (x == 200 and n == 2):
-                  lista_main[j][n] = 2
-            if y == 200 and j == 1:
-               if (x == 0 and n == 0) or (x == 100 and n == 1) or (x == 200 and n == 2):
-                  lista_main[j][n] = 2
-            if y == 300 and j == 2:
-               if (x == 0 and n == 0) or (x == 100 and n == 1) or (x == 200 and n == 2):
-                  lista_main[j][n] = 2
+   first_block(lista_main)
 
 
    #PLAY UNTIL EXIT
    while True:
       DrawBoard(0, width, 0, screen, black, 199)
-      screen.blit(text_render, (width/3, 10))
+      screen.blit(text_render, (width / 3, 10))
       screen.blit(text_map, (10, 40))
       screen.blit(text_score, (10, 60))
       screen.blit(text_time, (10, 80))
@@ -266,7 +334,7 @@ def main():
                ##ARROW - UP
 
             ##moving blocks
-               arrow_up(lista_main)
+               arrow_up(lista_main,screen)
                if safe != lista_main:
                   move = 1
             ##join blocks
@@ -278,7 +346,7 @@ def main():
                         points += lista_main[j][i]
                         move = 1
             ##repeat moving blocks
-               arrow_up(lista_main)
+               arrow_up(lista_main,screen)
 
             if event.key == pygame.K_DOWN:
 
@@ -330,39 +398,16 @@ def main():
                exit = 0
 
 
-            # loading images
-         for i in range(0, 3, 1):
-            for j in range(0, 3, 1):
-               position = checking_position(i,j)
-
-               if lista_main[i][j] == 2:
-                  screen.blit(image_2, position)
-               if lista_main[i][j] == 4:
-                  screen.blit(image_4, position)
-               if lista_main[i][j] == 8:
-                  screen.blit(image_8, position)
-               if lista_main[i][j] == 16:
-                  screen.blit(image_16, position)
-               if lista_main[i][j] == 32:
-                  screen.blit(image_32, position)
-               if lista_main[i][j] == 64:
-                  screen.blit(image_64, position)
-               if lista_main[i][j] == 128:
-                  screen.blit(image_128, position)
-               if lista_main[i][j] == 256:
-                  screen.blit(image_256, position)
-               if lista_main[i][j] == 512:
-                  screen.blit(image_512, position)
-               if lista_main[i][j] == 1024:
-                  screen.blit(image_1024, position)
-               if lista_main[i][j] == 2048:
-                  screen.blit(image_2048, position)
-                  screen.blit(image_win,(0,100))
+         # loading images
+         for i in range(3):
+            for j in range(3):
+               image(lista_main, screen, i, j)
 
          if END(screen, image_lose, lista_main) == True:
             screen.blit(image_lose, (1, 100))
 
       pygame.display.update()
+
 
 #call function
 main()
